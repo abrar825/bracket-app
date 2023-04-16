930c88f30619c4e204863e1456be114771dea3b4
Abrar Chowdhury <abrar825@gmail.com>
2023-04-16 15:30:12 -0400

    index.js update

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,24 @@
+const joi = require("joi");
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+
+let picks = [{ id: 1, name: "Abe", picks: "" }];
 
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.send(picks);
 });
+
+app.post("/", (req, res) => {
+  const pick = {
+    id: picks.length + 1,
+    name: req.body.name,
+    myPicks: req.body.myPicks,
+  };
+
+  picks.push(pick);
+  res.send(pick);
+});
+
 app.listen(3010, () => console.log("Listening..."));
